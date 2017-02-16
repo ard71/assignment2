1ea35be71c16a2433bb97121c92b39aa06543343
ard71 <ard71@pitt.edu>
2017-02-15 21:49:31 -0500

    Fix spacing

--- js/ramses.js
+++ js/ramses.js
@@ -2,7 +2,6 @@ function validateForm(){
 	var answer = document.getElementsByName('adorable');
 	if (answer[0].checked == true){
 		alert("Yes! He's the cutest.");
-		window.location.href="correct.html";
 	}
 	else{
 		alert("You're a monster!");
